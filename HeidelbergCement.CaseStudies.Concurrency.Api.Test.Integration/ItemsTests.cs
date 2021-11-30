4c2df3b36e7152661a2ae2f133db249575d25d6e
Arjmandi, Buzurgmehr (Heidelberg) DEU <buzurgmehr.arjmandi@heidelbergcement.com>
2021-11-30 17:16:47 +0100

    docs: Add comments to test

--- HeidelbergCement.CaseStudies.Concurrency.Api.Test.Integration/ItemsTests.cs
+++ HeidelbergCement.CaseStudies.Concurrency.Api.Test.Integration/ItemsTests.cs
@@ -36,18 +36,28 @@ public class ItemsTests: IntegrationTestBase
             .AddQueryParams("scheduleId", scheduleId);
         
         // Exercise
+        // First let's get the schedule before adding any items. This schedule is currently empty..
         var scheduleBeforeAddition = await draftScheduleRequest.Get<ScheduleResponseDto>();
+
         var scheduleId = scheduleBeforeAddition.ScheduleId.ToString();
         var addItemRequest = addItemForScheduleRequest(scheduleId);
+
+        // Simultaneously start two tasks that will make the same exact item addition request.
+        // This is a race condition, the first request should pass and the second should fail.
         var itemAddResponses = await Task.WhenAll(addItemRequest.Post(itemToAdd, false), addItemRequest.Post(itemToAdd, false));
+
+        //Finally let's get the schedule after the item addition requests. It should have only one item in it.
         var scheduleAfterAddition = await draftScheduleRequest.Get<ScheduleResponseDto>();
 
         // Verify
         scheduleBeforeAddition.ScheduleItems.Count.Should().Be(0);
+        
         var failures = itemAddResponses.ToList().Where(it => it.IsSuccessStatusCode == false);
         var successes = itemAddResponses.ToList().Where(it => it.IsSuccessStatusCode == true);
+
         failures.Count().Should().Be(1);
         successes.Count().Should().Be(1);
+        
         scheduleAfterAddition.ScheduleItems.Count.Should().Be(1);
     }
 }
